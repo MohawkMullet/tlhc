613c54c16b67d95358ae479288c7ff90c91fffd3
mohawkdigital <mohawkdigital@4b03fbda-bf05-11dd-b7f4-ab9653f8cbb3>
2008-12-10 12:52:56 +0000

    cheater tag

---
+++ trunk/partsOut/cheaterTag.php
@@ -0,0 +1,4 @@
+<div align="center"><img src=" http://i5.ebayimg.com/03/i/001/21/72/d5b0_1_bo.JPG"><br>
+<font face="Arial,Helvetica" size="2">Powered by <a href="http://code.google.com">TL html Cheater</a>.<br>
+The Free as in Freedom (GPLv3) HTML tool.  List your items fast and easy and manage your active items.</font></div>
+<hr>
