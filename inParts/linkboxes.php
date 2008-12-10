0952872c172e1197245d774a3f4cf32a00193544
mohawkdigital <mohawkdigital@4b03fbda-bf05-11dd-b7f4-ab9653f8cbb3>
2008-12-10 16:23:32 +0000

    moved for organization

--- inParts/linkboxes.php
+++
@@ -1,4 +0,0 @@
-<table><tr><td style="white-space:nowrap;">
-<input name="link" type="text" id="link" accesskey="i" tabindex="13" value="" size="16" />
--
-<input name="linkText" type="text" id="linkText" accesskey="l" tabindex="14" value="" size="24" /></td></tr></table>
