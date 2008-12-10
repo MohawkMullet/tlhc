1882a4224eb91fe6a27ff44f34420ddc3b5ed752
mohawkdigital <mohawkdigital@4b03fbda-bf05-11dd-b7f4-ab9653f8cbb3>
2008-12-10 23:32:25 +0000

    defined $listingMast instead of titler

--- trunk/inMain.php
+++ trunk/inMain.php
@@ -13,7 +13,7 @@
   <tr>
     <td class="RightHandLabels">&nbsp;</td>
     <td class="inputBGstyle">  
-<input type="text" name="performer" size="15" class="someinputs" id="performer"><input name="titler" type="text" class="someinputs" id="titler" accesskey="h" tabindex="1" onfocus="if(this.value=='TITLE')this.value='';" value="TITLE" size="30" maxlength="55" />  </td>
+<input type="text" name="performer" size="15" class="someinputs" id="performer"  tabindex="1"><input name="titler" type="text" class="someinputs" id="titler" accesskey="h" tabindex="2" onfocus="if(this.value=='TITLE')this.value='';" value="TITLE" size="30" maxlength="55" />  </td>
   </tr>
 
 <?php
@@ -44,12 +44,12 @@
   </tr>
   <tr>
     <td class="RightHandLabels">Abrs.</td>
-    <td class="inputBGstyle"><input name="notes" type="text" class="someinputs" id="notes" accesskey="n" tabindex="11" size="20" maxlength="90" /></td>
+    <td class="inputBGstyle"><input name="notes" type="text" class="someinputs" id="notes" accesskey="n" tabindex="14" size="20" maxlength="90" /></td>
   </tr>
   <tr>
     <td class="RightHandLabels">bar</td>
     <td class="inputBGstyle">
-    <textarea name="sideBar2" class="someinputs" cols="38" rows="3" wrap="physical" id="sideBar2" accesskey="s" tabindex="12"></textarea></td>
+    <textarea name="sideBar2" class="someinputs" cols="38" rows="3" wrap="physical" id="sideBar2" accesskey="s" tabindex="15"></textarea></td>
   </tr>
   <tr>
     <td class="RightHandLabels">Link</td>
@@ -63,7 +63,7 @@
   </tr>
     <tr>
     <td class="RightHandLabels">Picasa</td>
-    <td class="inputBGstyle"><input name="picasaImage" type="text" size="45"></td>
+    <td class="inputBGstyle"><input name="picasaImage" type="text" size="45"  tabindex="18"></td>
   </tr>
     <tr><td class="RightHandLabels">&nbsp;</td>
     <td class="inputBGstyle">
