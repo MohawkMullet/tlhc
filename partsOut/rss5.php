e1e39469c6e5dac7442ed8ab6d2d553ed67c840c
mohawkdigital <mohawkdigital@4b03fbda-bf05-11dd-b7f4-ab9653f8cbb3>
2008-12-10 12:53:13 +0000

    nada new

--- partsOut/rss5.php
+++ partsOut/rss5.php
@@ -16,7 +16,7 @@ $url = "http://rss.api.ebay.com/ws/rssapi?FeedName=StoreItems&siteId=0&language=
 //	$url = $_GET['url'];
 //$num_items = 10;
 $rss = fetch_rss($url);
-//$items = array_slice($rss->items, 0, $num_items);
+//$items = array_slice($rss->items, 5, $num_items);
 //	echo "Channel Title: " . $rss->channel['title'] . "<p>";
 	echo "<table style=\"font-size: 9px;\" align=\"center\"><tr>";
 	echo "<td><img src=\"http://lh4.ggpht.com/_W_e1PG0tXUk/ST9luwR54sI/AAAAAAAAAQ0/nVTnXiR3vro/s144/more45s.png\" /></td>";
