cc0efef5f13f19dbd1c7c0c16688010a3cdffee1
mohawkdigital <mohawkdigital@4b03fbda-bf05-11dd-b7f4-ab9653f8cbb3>
2008-12-10 23:33:18 +0000

    some changes

--- partsIn/LblNoSlvIN.php
+++ partsIn/LblNoSlvIN.php
@@ -1,5 +1,5 @@
   <tr>
     <td class="RightHandLabels">Lbl</td>
-    <td class="inputBGstyle"><input name="Label" type="text" class="someinputs" id="Label" accesskey="l" tabindex="4" size="20" />
-	&nbsp;&nbsp;<input name="recNum" type="text" class="someinputs" id="recNum" accesskey="h" tabindex="5" onfocus="if(this.value=='No.')this.value='';" value="No." size="10" /> 	&nbsp;Sleeve?<input name="origSleeve" type="checkbox" value="origSleeve"></td>
+    <td class="inputBGstyle"><input name="Label" type="text" class="someinputs" id="Label" accesskey="l" tabindex="5" size="20" />
+	&nbsp;&nbsp;<input name="recNum" type="text" class="someinputs" id="recNum" accesskey="h" tabindex="6" onfocus="if(this.value=='No.')this.value='';" value="No." size="10" /> 	&nbsp;Sleeve?<input name="origSleeve" type="checkbox" value="origSleeve"  tabindex="7"></td>
   </tr>
--- partsIn/linkboxes.php
+++ partsIn/linkboxes.php
@@ -1,2 +1,2 @@
-<input name="link" type="text" id="link" accesskey="i" tabindex="13" value="" size="16" />&nbsp;
-<input name="linkText" type="text" id="linkText" accesskey="l" tabindex="14" value="" size="24" />
+<input name="link" type="text" id="link" accesskey="i" tabindex="16" value="" size="16" />&nbsp;
+<input name="linkText" type="text" id="linkText" accesskey="l" tabindex="17" value="" size="24" />
