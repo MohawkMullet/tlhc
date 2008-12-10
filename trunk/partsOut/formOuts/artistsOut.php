a236736a42065981d5dfed7d15290cba478cf50e
mohawkdigital <mohawkdigital@4b03fbda-bf05-11dd-b7f4-ab9653f8cbb3>
2008-12-10 12:25:52 +0000

    needed to be all caps

--- trunk/partsOut/formOuts/artistsOut.php
+++ trunk/partsOut/formOuts/artistsOut.php
@@ -1,6 +1,6 @@
 <?php
 		$artists = $_POST['artists'];
-		$artists = ucwords(strtolower($artists));
+		$artists = ucwords(strtoupper($artists));
 		if ($artists != "") {
 				echo "<span class=\"greyFontLarge\">Artists:&nbsp;&nbsp;</span>";		
 				echo "<span class=\"blueFont18\"><strong>$artists</strong></span>";
