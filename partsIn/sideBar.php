d61cd4f1994094775544adc69d56259010666a5b
mohawkdigital <mohawkdigital@4b03fbda-bf05-11dd-b7f4-ab9653f8cbb3>
2008-12-12 00:21:57 +0000

    cleanup of junk temp test files

--- partsIn/sideBar.php
+++
@@ -1,5 +0,0 @@
-  <tr>
-    <td class="RightHandLabels">bar</td>
-    <td class="inputBGstyle">
-    <textarea name="sideBar2" class="someinputs" cols="38" rows="3" wrap="physical" id="sideBar2" accesskey="s" tabindex="15"></textarea></td>
-  </tr>
