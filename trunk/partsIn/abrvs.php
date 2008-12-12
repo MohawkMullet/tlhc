e3e34157d7bdda2c7b6fd9a8c0bd0a36f67d7025
mohawkdigital <mohawkdigital@4b03fbda-bf05-11dd-b7f4-ab9653f8cbb3>
2008-12-12 00:21:57 +0000

    cleanup of junk temp test files

--- trunk/partsIn/abrvs.php
+++
@@ -1,4 +0,0 @@
-<tr>
-    <td class="RightHandLabels"><a href="#" style="text-decoration:none; color:#FFFFFF;">Abrs.</a></td>
-    <td class="inputBGstyle"><input name="notes" type="text" class="someinputs" id="notes" accesskey="n" tabindex="14" size="20" maxlength="90" /></td>
-</tr>
