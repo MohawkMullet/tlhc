89f168fcd4342c400b1a3abc9e4e30ba3934e060
mohawkdigital@gmail.com <mohawkdigital@gmail.com@4b03fbda-bf05-11dd-b7f4-ab9653f8cbb3>
2008-12-13 09:42:14 +0000

    added install doc for easy setup

---
+++ trunk/docs/easyInstall.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>Untitled Document</title>
+<style type="text/css">
+<!--
+.style3 {color: #CC0000}
+-->
+</style>
+</head>
+
+<body>
+<h1>INSTALLATION
+</h1>
+<h3 class="style3">DO NOT INSTALL THESE FILES ON A PRODUCTION WEBSERVER.</h3>
+<p>These scripts are meant to be run on a private webserver. </p>
+<ol>
+  <li>copy all the files in the main directory to a new directory in your public_html folder.</li>
+  <li>create a mysql database called tlhc</li>
+  <li>create a mysql user named tlhcuser with the password of tlhcpass and full permissions on the new database you created above.</li>
+  <li>run the included .sql file found in the INSTALL folder.</li>
+  <li>fire up your web browser and point it to your new folder. </li>
+</ol>
+</body>
+</html>
