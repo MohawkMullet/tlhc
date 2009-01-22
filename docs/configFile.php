b16ce26a1c4b0f4859f6b6f69b62f067fdf85b2a
mohawkdigital <mohawkdigital@4b03fbda-bf05-11dd-b7f4-ab9653f8cbb3>
2009-01-22 00:32:19 +0000

    documentation has been improved

---
+++ docs/configFile.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>Untitled Document</title>
+</head>
+
+<body>
+ <h1>Editing the Config File</h1>
+ <p> The config file is located at /parts/config.php</p>
+ <p>You need to edit several values in this file to make the scripts in TLhC ouput the correct information for your listings.</p>
+ <p>Store Name: This is where you are going to put in the name of your ebay store. Example: If your store was named: &quot;My Used Shoes&quot; you would change the line to look like this</p>
+ <p>$storeName = &quot;My Used Shoes&quot;;</p>
+ <p>Basically just type your store name between the quotes &quot; &quot; and leave the quotes and the semicolon and the end.</p>
+ <p>
+   <?php
+include('footer.php');
+?>
+ </p>
+ <p>&nbsp; </p>
+</body>
+</html>
