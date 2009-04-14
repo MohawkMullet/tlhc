5bee09ce411f79ee96c1009d2f65ced6b43c2718
mohawkdigital <mohawkdigital@4b03fbda-bf05-11dd-b7f4-ab9653f8cbb3>
2009-04-14 08:22:55 +0000

    this shit is long gone.  all into one file.  finally.  i struggled with learing but I'm getting there lately.

--- parts/db/label.php
+++
@@ -1,6 +0,0 @@
-  <form method="POST" action="parts/db/labelOut3.php" target="mainFrame" name="labelAdd" id="labelAdd">
-<table><tr>
-<td width="25px" style="text-align:right;"><span class="style1">Label</span></td>
-<td width="144px"><input type="text" name="labelValue" onfocus="if(this.value=='shows blah blah')this.value='';" value="shows blah blah"></td>
-<td width="100px"><input type="submit" value="Label">&nbsp;<input name="clearme" type="reset" id="clearme" tabindex="15" value="Cl" /></td></tr></table>
-</form>
--- parts/db/vinyl.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="parts/db/vinylOut3.php" target="mainFrame" name="vinylAdd" id="vinylAdd">
-<table><tr><td width="25px">&nbsp;</td>
-<td width="144px"><input type="text" name="vinylValue"></td>
-<td width="100px"><input type="submit" value="Vinyl">&nbsp;<input name="clearme" type="reset" id="clearme" tabindex="15" value="Cl" /></td></tr></table>
-</form>
