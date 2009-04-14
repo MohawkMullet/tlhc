890b6ed689c3c196648113ee7324346405b927f2
mohawkdigital <mohawkdigital@4b03fbda-bf05-11dd-b7f4-ab9653f8cbb3>
2009-04-14 08:22:55 +0000

    this shit is long gone.  all into one file.  finally.  i struggled with learing but I'm getting there lately.

--- trunk/parts/db/misc.php
+++
@@ -1,6 +0,0 @@
-<form method="POST" action="parts/db/miscOut3.php" target="mainFrame" name="miscAdd" id="miscAdd">
-<table><tr>
-<td width="25px" style="text-align:right;"><span class="style1">Miscellanious</span></td>
-<td width="144px"><input type="text" name="miscValue"></td>
-<td width="100px"><input type="submit" value="Misc">&nbsp;<input name="clearme" type="reset" id="clearme" tabindex="15" value="Cl" /></td></tr></table>
-</form>
