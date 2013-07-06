cb658f8a1c586f8ebf0597610320167e53af4e4e
mohawkdigital <mohawkdigital@gmail.com>
2013-07-06 10:36:53 +0000

    Just adding some new docs to the docs folder.  Trying to get a real installation guide and actual online help fixed

---
+++ docs/test.php
@@ -0,0 +1,6 @@
+<a href="index.php">Back To Index</a>
+
+<?php
+//include_once('../license.php');
+?>
+<table style="width:194px;"><tr><td align="center" style="height:194px;background:url(https://picasaweb.google.com/s/c/transparent_album_background.gif) no-repeat left"><a href="https://picasaweb.google.com/117984535952369497366/45s?authuser=0&feat=embedwebsite"><img src="https://lh3.googleusercontent.com/-9CtyZKoQmrA/SL-Hnnrm8fE/AAAAAAAAE4I/u8HHKuTrh2A/s160-c/45s.jpg" width="160" height="160" style="margin:1px 0 0 4px;"></a></td></tr><tr><td style="text-align:center;font-family:arial,sans-serif;font-size:11px"><a href="https://picasaweb.google.com/117984535952369497366/45s?authuser=0&feat=embedwebsite" style="color:#4D4D4D;font-weight:bold;text-decoration:none;">45s</a></td></tr></table>
